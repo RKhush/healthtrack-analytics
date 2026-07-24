5ddbb86fc351601217126ea2d34a5de3da078386
krathva707 <c0947900@mylambton.ca>
2026-07-24 13:55:08 -0400

    fix: allow all origins for CORS

--- HealthTrack.API/Program.cs
+++ HealthTrack.API/Program.cs
@@ -72,26 +72,15 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(connectionString);
 });
 
-
-
-
-
-
-
-
-
 // CORS
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowReact", policy =>
-        policy.WithOrigins(
-            "http://localhost:5173",
-            "https://healthtrack-analytics.vercel.app",
-            "https://healthtrack-analytics-git-main-khusie.vercel.app"
-        )
-        .AllowAnyHeader()
-        .AllowAnyMethod());
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod());
 });
+
 
 var app = builder.Build();
 
