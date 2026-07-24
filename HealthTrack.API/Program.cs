2fd452e36bbbfa9c15473eb28e5866073a096588
krathva707 <c0947900@mylambton.ca>
2026-07-24 12:56:52 -0400

    fix : enable swagger in production

--- HealthTrack.API/Program.cs
+++ HealthTrack.API/Program.cs
@@ -88,11 +88,9 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseCors("AllowReact");
 app.UseHttpsRedirection();
